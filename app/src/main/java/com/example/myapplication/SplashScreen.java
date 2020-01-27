cade1cbd006d7e0fa40546790dc9625bb1cb7938
emaz <emaz111@hotmail.com>
2020-01-27 16:20:52 +0000

    Splash Screen

--- app/src/main/java/com/example/myapplication/SplashScreen.java
+++ app/src/main/java/com/example/myapplication/SplashScreen.java
@@ -1,4 +1,27 @@
 package com.example.myapplication;
 
-public class SplashScreen {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(splashIntent);
+                finish();
+            }
+        },3000);
+    }
 }
+
