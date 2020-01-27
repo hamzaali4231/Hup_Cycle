45e74bf40ce5035f6a664bcf770ae29eae1d87dd
hamza <u1763290@unimail.hud.ac.uk>
2020-01-27 21:48:47 +0000

    Should work now

--- app/src/main/java/com/example/myapplication/SplashScreen.java
+++ app/src/main/java/com/example/myapplication/SplashScreen.java
@@ -3,29 +3,28 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity implements Runnable{
 
-    private final static int Delay=3000;
-
-
+    private final static int Delay=5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
         Handler splashHandler= new Handler();
         splashHandler.postDelayed(this,Delay);
+
     }
 
     @Override
     public void run() {
         Intent splashIntent = new Intent(SplashScreen.this, MainActivity.class);
-        SplashScreen.this.startActivity(splashIntent);
-        SplashScreen.this.finish();
+        startActivity(splashIntent);
+       finish();
     }
 }
 
