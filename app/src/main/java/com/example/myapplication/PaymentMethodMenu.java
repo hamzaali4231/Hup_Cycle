a6ce372cb8804022525f4c9f43e9e2bff5febe35
hamza <u1763290@unimail.hud.ac.uk>
2020-03-16 12:31:03 +0000

    Cash payment

--- app/src/main/java/com/example/myapplication/PaymentMethodMenu.java
+++ app/src/main/java/com/example/myapplication/PaymentMethodMenu.java
@@ -4,9 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Arrays;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PaymentMethodMenu extends AppCompatActivity {
@@ -24,17 +30,30 @@ public class PaymentMethodMenu extends AppCompatActivity {
         cardButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), CardPaymentActivity.class);
-                startActivity(intent);
+                cashPayment();
             }
         });
 
         cashButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent( getApplicationContext(), Sell.class);
-                startActivity(intent);
+               cashPayment();
             }
         });
+    }
+
+    public void cashPayment() {
+        FirebaseDatabase.getInstance().getReference().child("Cart List")
+                .child("User View").removeValue().addOnCompleteListener
+                (new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent in = new Intent(PaymentMethodMenu.this, Buy.class);
+                        Toast.makeText(PaymentMethodMenu.this, "Items reserved", Toast.LENGTH_SHORT).show();
+                        startActivity(in);
+                        finish();
+                    }
+                });
+
     }
 }
